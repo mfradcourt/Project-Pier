3055d5a974f0583742fef109bbfcfddde25eff2a
Matthieu Fradcourt <mfradcourt@gmail.com>
2013-01-27 02:17:09 +0100

    FIX php notice

--- index.php
+++ index.php
@@ -7,6 +7,6 @@
   if (!version_compare(phpversion(), '5.0', '>=')) {
     die('<strong>Installation error:</strong> in order to run ProjectPier you need PHP5. Your current PHP version is: ' . phpversion());
   } // if
-  define('PUBLIC_FOLDER', 'public');
+  if(!defined('PUBLIC_FOLDER')) define('PUBLIC_FOLDER', 'public');
   require 'init.php';
 ?>
